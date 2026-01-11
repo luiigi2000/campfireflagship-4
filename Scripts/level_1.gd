4881d27359093f160f78b1c7c3ea1f30fc9c525a
v=dQw4w9WgXcQ <zacharytgarland@gmail.com>
2026-01-11 02:12:16 -0500

    Mouse Update
    I found some pixel art and changed around the mouse art.

--- Scripts/level_1.gd
+++ Scripts/level_1.gd
@@ -2,35 +2,37 @@ extends Node2D
 
 var current_sequence = []
 var all_notes = [preload("res://Audio/Scene1/a.wav"),preload("res://Audio/Scene1/b.wav"),preload("res://Audio/Scene1/c.wav"),preload("res://Audio/Scene1/d.wav"),preload("res://Audio/Scene1/d#.mp3"),preload("res://Audio/Scene1/e.mp3")]
-var correct_sequence = [all_notes[5],all_notes[4],all_notes[5],all_notes[4],all_notes[5],all_notes[1],all_notes[3],all_notes[2],all_notes[0]]
-
+var correct_sequence = [all_notes[5],all_notes[4],all_notes[5],all_notes[4],all_notes[5],all_notes[1],all_notes[3],all_notes[2],all_notes[0]] 
 func _ready() -> void:
-	
+	for button in get_tree().get_nodes_in_group("Buttons"):
+		button.connect("mouse_entered", Callable(self, "_on_button_hover"))
+		button.connect("mouse_exited", Callable(self, "_on_button_exit"))
+
 	assign_note_sequence()
 
 func _on_string_pressed() -> void:
-	play_string($PianoKeys/Key/KeyAudio)
+	play_key($PianoKeys/Key/KeyAudio)
 
 
 func _on_string_2_pressed() -> void:
-	play_string($PianoKeys/Key2/Key2Audio)
+	play_key($PianoKeys/Key2/Key2Audio)
 
 
 func _on_string_3_pressed() -> void:
-	play_string($PianoKeys/Key3/Key3Audio)
+	play_key($PianoKeys/Key3/Key3Audio)
 
 
 func _on_string_4_pressed() -> void:
-	play_string($PianoKeys/Key4/Key4Audio)
+	play_key($PianoKeys/Key4/Key4Audio)
 
 
 func _on_string_5_pressed() -> void:
-	play_string($PianoKeys/Key5/Key5Audio)
+	play_key($PianoKeys/Key5/Key5Audio)
 	
 func _on_string_6_pressed() -> void:
-	play_string($PianoKeys/Key6/Key6Audio)
+	play_key($PianoKeys/Key6/Key6Audio)
 
-func play_string(piano_key):
+func play_key(piano_key):
 	piano_key.play()
 	check_sequence(piano_key)
 	
@@ -44,7 +46,6 @@ func assign_note_sequence():
 		key.get_node(key.name+"Audio").stream = all_notes[index]
 		index += 1
 	
-var count=0
 func check_sequence(piano_note):
 	var sequence_correct := true
 	current_sequence.push_back(piano_note.stream)
@@ -52,20 +53,19 @@ func check_sequence(piano_note):
 		if current_sequence[note_index] != correct_sequence[note_index]:
 			sequence_correct = false
 			current_sequence = []
-	if sequence_correct:
-		print(count)
-		count+=1
 		
 	if current_sequence.size() != correct_sequence.size():
 		sequence_correct = false
 	if sequence_correct:
 		$"../Goal".modulate = Color.GREEN
 		current_sequence = []
-		await get_tree().create_timer(3).timeout
 		Main.state = Main.level.level2
 		Main.change_level()
 		queue_free()
 		
-			
-	
-	
+func _on_button_hover():
+	# button is the one hovered
+	Input.set_custom_mouse_cursor(preload("res://Mouse/whitehandhover.png"))
+
+func _on_button_exit():
+	Input.set_custom_mouse_cursor(preload("res://Mouse/whitehandmove.png"))
